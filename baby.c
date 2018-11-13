21b0d620431604e1604bd66db9206f54202d871a
RyanCrampton <37187637+RyanCrampton@users.noreply.github.com>
2018-11-13 18:43:29 +0000

    Add files via upload

---
+++ baby.c
@@ -0,0 +1,52 @@
+int incriment()
+{
+
+}
+int fetch()
+{
+
+}
+int decode()
+{
+
+}
+int execute()
+{
+	if()
+}
+int display_everything()
+{
+
+}
+int JMP()
+{
+
+}
+int JRP()
+{
+
+}
+int LDN()
+{
+
+}
+int STO()
+{
+
+}
+int SUB()
+{
+
+}
+int CMP()
+{
+
+}
+int STP()
+{
+	
+}
+int main()
+{
+
+}
